c0007e53b99522ff9cf68080a46ffaaf45ae0083
Руслан Русланов <sharp_blade666@mail.ru>
2021-09-18 23:30:33 +0300

    bad version

--- Homewrok/Homework1/Homework1/Program.cs
+++ Homewrok/Homework1/Homework1/Program.cs
@@ -2,11 +2,31 @@
 
 namespace Homework1
 {
-    internal class Program
+    internal static class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Yo!");
+            var result = 0;
+            var val1 = int.Parse(args[0]);
+            
+            var operation = args[1];
+            var val2 = int.Parse(args[2]);
+            switch (operation)
+            {
+                case "+":
+                    result = val1 + val2; break;
+                case "-":
+                    result = val1 - val2;
+                    break;
+                case "*": 
+                    result = val1 * val2;
+                    break;
+                case "/": 
+                    result = val1 / val2;
+                    break;
+            }
+            
+            Console.WriteLine($"{args[0]}{args[1]}{args[2]}={result}");
         }
     }
 }
