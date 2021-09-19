1a317ed9260c216ba5593475fa46ac1f245033d6
Руслан Русланов <sharp_blade666@mail.ru>
2021-09-19 19:06:30 +0300

    100% coverage for TryToParse and refactor

--- Homewrok/Homework1/Homework1.Tests/ParserTests.cs
+++ Homewrok/Homework1/Homework1.Tests/ParserTests.cs
@@ -1,5 +1,4 @@
 ﻿using Xunit;
-using System;
 
 namespace Homework1.Tests
 {
@@ -7,12 +6,39 @@ namespace Homework1.Tests
     {
         [Theory]
         [InlineData(new[] {"2","+","3"},0)]
-        [InlineData(new[] {"2.0","+","3"},1)]
-        [InlineData(new[] {"2","%","3"},2)]
-        public void TryToParse_IsParsable(string[] args, int expected)
+        [InlineData(new[] {"2","-","3"},0)]
+        [InlineData(new[] {"2","*","3"},0)]
+        [InlineData(new[] {"2","/","3"},0)]
+        public void TryToParse_Return0(string[] args, int expected)
         {
             //Act
-            var res = Parser.TryToParse(args, out var val1, out var operation, out var val2);
+            var res = Parser.TryToParse(args, out _, out _, out _);
+            
+            //Assert
+            Assert.Equal(expected,res);
+        }
+        
+        [Theory]
+        [InlineData(new[] {"2.0","+","3"},1)]
+        [InlineData(new[] {"2.0","*","asdas"},1)]
+        [InlineData(new[] {"0","/",""},1)]
+        public void TryToParse_Return1(string[] args, int expected)
+        {
+            //Act
+            var res = Parser.TryToParse(args, out _, out _, out _);
+            
+            //Assert
+            Assert.Equal(expected,res);
+        }
+        
+        [Theory]
+        [InlineData(new[] {"2","%","3"},2)]
+        [InlineData(new[] {"2","2","3"},2)]
+        [InlineData(new[] {"2"," ","3"},2)]
+        public void TryToParse_Return2(string[] args, int expected)
+        {
+            //Act
+            var res = Parser.TryToParse(args, out _, out _, out _);
             
             //Assert
             Assert.Equal(expected,res);
