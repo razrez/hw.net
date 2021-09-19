714e26e2e32cca8ab0f8d282d6895e3ce12a48f2
Руслан Русланов <sharp_blade666@mail.ru>
2021-09-19 19:20:16 +0300

    ProgramTests.cs 100% coverage

--- Homewrok/Homework1/Homework1/Parser.cs
+++ Homewrok/Homework1/Homework1/Parser.cs
@@ -20,7 +20,7 @@ namespace Homework1
             operation = args[1];
             var isVal2Int = int.TryParse(args[2], out val2);
 
-            if (!isVal1Int || !isVal2Int) 
+            if (!isVal1Int || !isVal2Int) //if there are no int args
             {
                 Console.WriteLine($"{args[0]}{args[1]}{args[2]} is not a valid calculation syntax");
                 return 1;
@@ -32,7 +32,7 @@ namespace Homework1
                 $"{args[0]}{args[1]}{args[2]} is not a valid calculation syntax. "
                 + $"Supported operations are "
                 + $"{ExpectedOperation.Aggregate((c, n) => $"{c} {n}")}");
-            return 2;
+            return 2;//if unsupported operation
 
         }
     }
