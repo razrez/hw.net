8dc8d490b1a793f572562bc088cee7994426102a
Руслан Русланов <sharp_blade666@mail.ru>
2021-09-23 00:44:09 +0300

    2k-249 initial

---
+++ hw2/hw1.Tests/CalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace hw1.Tests
+{
+    public class CalculatorTests
+    {
+        [Theory]
+        [InlineData(2, "+", 2, 4)]
+        [InlineData(2, "-", 3, -1)]
+        [InlineData(3, "-", 2, 1)]
+        [InlineData(2, "*", 1, 2)]
+        [InlineData(6, "/", 6, 1)]
+        [InlineData(3, "/", 6, 0)]
+        public void Calculate_Operation_IsCorrect(int val1, string operation, int val2, int expected)
+        {
+            //Act
+            var result = Calculator.Calculate(val1, operation, val2);
+            
+            //Assert
+            Assert.Equal(expected,result);
+        }
+        
+        [Theory]
+        [InlineData(2, "", 2, 0)]
+        [InlineData(2, "sad", 3, 0)]
+        [InlineData(2, "3", 3, 0)]
+        public void Calculate_Operation_IsNotCorrect(int val1, string operation, int val2, int expected)
+        {
+            //Act
+            var result = Calculator.Calculate(val1, operation, val2);
+            
+            //Assert
+            Assert.Equal(expected,result);
+        }
+    }
+}
