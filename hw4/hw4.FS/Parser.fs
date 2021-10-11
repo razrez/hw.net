93f0fbb6ff9ae266d3496e417cdb821faebb3d97
Руслан Русланов <sharp_blade666@mail.ru>
2021-10-11 18:49:06 +0300

    Parser works bad

---
+++ hw4/hw4.FS/Parser.fs
@@ -0,0 +1,18 @@
+﻿module hw4.FS.Parser
+    let IsExpectedOperation =
+        [
+            "+";
+            "-";
+            "*";
+            "/"
+        ]
+        
+            
+    let TryToParse (args:string[]) =
+        if List.contains args.[1] IsExpectedOperation = false then 2 //if unsupported operation
+        
+        else if (box args.[0] :? int = false &&
+                 box args.[2] :? int = false
+                 || args.[1] = "/" && args.[2] = "0")
+                 then 1 //if there are no int args or DivideByZero
+        else 0
--- hw4/hw4.FS/Program.fs
+++ hw4/hw4.FS/Program.fs
@@ -1,17 +1,17 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
+open System.Collections.Generic
 open hw4.FS
 
 // Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let from whom = sprintf "from %s" whom
 
 [<EntryPoint>]
 let main argv =
     let message = from "F#" // Call the function
-    let res = Calculator.Calculate 2 "*" 8
+    let res = Parser.TryToParse argv
     printfn "Hello world %s" message
-    printfn $"{res}" 
-    
+    printfn $"{System.Int32.TryParse argv.[0] }"
+
     0 // return an integer exit code
