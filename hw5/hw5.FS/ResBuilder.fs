69b5d4a8d05dcf84978e40f78dba4bfe54d34a12
razrez <70781439+razrez@users.noreply.github.com>
2021-10-19 13:36:55 +0300

    Delete ResBuilder.fs

--- hw5/hw5.FS/ResBuilder.fs
+++
@@ -1,9 +0,0 @@
-﻿module HW5.ResBuilder
-type ResultBuilder() =
-    member this.Zero() = Error "Arguments have error(s)"
-    member this.Bind(x, f) =
-        match x with    
-        | Ok x -> f x
-        | Error error -> Error error
-    member this.Return x = Ok x
-let res = ResultBuilder()
