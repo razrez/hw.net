d5dab269bb7505cec0df7755285d5c4662a883a1
Руслан Русланов <sharp_blade666@mail.ru>
2021-10-19 15:06:34 +0300

    Parser and Calculator

--- hw5/hw5.FS/ResBuilder.fs
+++ hw5/hw5.FS/ResBuilder.fs
@@ -1,9 +1,18 @@
-﻿module HW5.ResBuilder
-type ResultBuilder() =
-    member this.Zero() = Error "Arguments have error(s)"
+﻿module hw5.ResBuilder
+type Operation =
+    | Plus = 0
+    | Minus = 1
+    | Multiply = 2
+    | Divide = 3
+
+////////////////////////////////////////////////////////           
+type ResBuilder() =
     member this.Bind(x, f) =
         match x with    
         | Ok x -> f x
         | Error error -> Error error
     member this.Return x = Ok x
-let res = ResultBuilder()
+    member this.Zero() = Error "Wrong Arguments!"
+    
+let res = ResBuilder()
+let (>>=) x f = Result.bind f x
