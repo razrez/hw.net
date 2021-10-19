4ff3f571ae2b89106d7ec57fe06db3c7d946a811
Руслан Русланов <sharp_blade666@mail.ru>
2021-10-19 13:05:42 +0300

    ResBuilder.fs

---
+++ hw5/hw5.FS/ResBuilder.fs
@@ -0,0 +1,9 @@
+﻿module HW5.ResBuilder
+type ResultBuilder() =
+    member this.Zero() = Error "Arguments have error(s)"
+    member this.Bind(x, f) =
+        match x with    
+        | Ok x -> f x
+        | Error error -> Error error
+    member this.Return x = Ok x
+let res = ResultBuilder()
