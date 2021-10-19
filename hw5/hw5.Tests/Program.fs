37e5b642755f4e81149707881a50ce846b3bedd5
Руслан Русланов <sharp_blade666@mail.ru>
2021-10-19 21:08:06 +0300

    adding actions

--- hw5/hw5.Tests/Program.fs
+++
@@ -1,7 +0,0 @@
-open Xunit
-[<Fact>]
-let Main_ReturnNot0 () =
-    Assert.True([|"228";"2";"%%"|] |> Program.main <> 0)
-[<Fact>]
-let Main_Return0 () =
-    Assert.True([|"16.5";"+";"102"|] |> Program.main = 0)  
--- hw5/hw5.Tests/ProgramTests.fs
+++ hw5/hw5.Tests/ProgramTests.fs
@@ -1,2 +1,8 @@
-﻿module ProgramTests
-
+﻿module hw5.Tests.ProgramTests
+open Xunit
+[<Fact>]
+let Main_ReturnNot0 () =
+    Assert.True([|"228";"2";"%%"|] |> Program.main <> 0)
+[<Fact>]
+let Main_Return0 () =
+    Assert.True([|"16.5";"+";"102"|] |> Program.main = 0)  
