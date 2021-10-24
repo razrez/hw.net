0c3bb13993de65ef62e0f972fa13d3c21565f71c
Руслан Русланов <sharp_blade666@mail.ru>
2021-10-24 18:49:26 +0300

    tests proj created

--- hw6/RequestApp/Program.fs
+++ hw6/RequestApp/Program.fs
@@ -2,7 +2,7 @@ open System.Net
 open FSharp.Data
 let doRequest =
     async {
-        let! r1 = Http.AsyncRequest "https://localhost:5001/calculate?arg1=5&operation=*&arg2=7"
+        let! r1 = Http.AsyncRequest "https://localhost:5001/calculate?arg1=523114&operation=/&arg2=67"
         let str =
             match r1.Body with
             | HttpResponseBody.Text t -> t
