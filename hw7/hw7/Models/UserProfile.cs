b51677ad44bbe343e1ecf492592620b85f40de29
Руслан Русланов <sharp_blade666@mail.ru>
2021-11-08 00:57:28 +0300

    ValidationAttributes were used

--- hw7/hw7/Models/UserProfile.cs
+++ hw7/hw7/Models/UserProfile.cs
@@ -1,9 +1,32 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace hw7.Models
 {
-    public record UserProfile([Required] string? FirstName, string? LastName, Sex Sex, int Age)
+    public record UserProfile
     {
+        [Required(ErrorMessage = "УКАЖИТЕ ИМЯ ПЖ")]
+        [MaxLength(20, ErrorMessage = "20 символов - максимум")]
+        [DisplayName("Имя")]
+        public string FirstName { get; set; }
+        
+        [Required(ErrorMessage = "УКАЖИТЕ ФАМИЛИЮ ПЖ")]
+        [MaxLength(30, ErrorMessage = "30 символов - максимум!")]
+        [DisplayName("Фамилия")]
+        public string LastName { get; set; }
+        
+        [MaxLength(30, ErrorMessage = "30 символов - максимум!")]
+        [DisplayName("Любимое блюдо =)")]
+        public string FavouriteMeal { get; set; }
+        
+        [Required(ErrorMessage = "УКАЖИТЕ ВОЗРАСТ ПЖ")]
+        [Range(12, 128, ErrorMessage = "Допустимый возраст: от 12 до 128 лет!")]
+        [DisplayName("Возраст")]
+        public int Age { get; set; }
+        
+        [Required(ErrorMessage = "УКАЖИТЕ ГЕНДЕР ПЖ")]
+        [DisplayName("Пол")]
+        public Sex Sex { get; set; }
     }
 
     public enum Sex
