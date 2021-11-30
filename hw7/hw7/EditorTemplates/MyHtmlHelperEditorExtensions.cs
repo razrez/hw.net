de24a304fc7a47d0ff551a60ffbede9692926619
Руслан Русланов <sharp_blade666@mail.ru>
2021-11-30 22:43:53 +0300

    let's go...

---
+++ hw7/hw7/EditorTemplates/MyHtmlHelperEditorExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace hw7.wwwroot
+{
+    public static class MyHtmlHelperEditorExtensions
+    {
+        public static IHtmlContent MyEditorForModel(this IHtmlHelper htmlHelper)
+        {
+            if (htmlHelper == null)
+            {
+                throw new ArgumentNullException(nameof(htmlHelper));
+            }
+            // кароче мне нужно получить все поля и в зависимости от типа данных, правильно их преобразовать
+            // привет, рефлексия
+            
+            return null;
+        }
+    }
+}
