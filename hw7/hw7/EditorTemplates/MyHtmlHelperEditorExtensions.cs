2b4daed6552a35aa2987548e05eac98057f4a670
Руслан Русланов <sharp_blade666@mail.ru>
2021-12-07 01:32:34 +0300

    AddHtmlContent isn't done

--- hw7/hw7/EditorTemplates/MyHtmlHelperEditorExtensions.cs
+++ hw7/hw7/EditorTemplates/MyHtmlHelperEditorExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using hw7.Models;
 using Microsoft.AspNetCore.Html;
@@ -15,16 +16,24 @@ namespace hw7.wwwroot
             {
                 throw new ArgumentNullException(nameof(htmlHelper));
             }
-            
-            var type = htmlHelper.ViewData.ModelMetadata.ModelType;
-            var tagbuilder = new TagBuilder("li");
-            //tagbuilder.Attributes.Ad
-            return null;
+            var typeModel = htmlHelper.ViewData.Model.GetType();
+            IHtmlContentBuilder htmlRes = new HtmlContentBuilder();
+            var model = htmlHelper.ViewData.Model;
+            foreach (var property in typeModel.GetProperties())
+                AddHtmlContent(htmlRes, property, model);
+                //model нужна для сравнения и определения типа инпута
+            return htmlRes;
         }
+
+        private static void AddHtmlContent(IHtmlContentBuilder htmlRes, PropertyInfo property, object model)
+        {
+            throw new NotImplementedException();
+        }
+
         public static IHtmlContent CreateSubmit(this IHtmlHelper helper, string name, string value)
         {
             var btn = new StringBuilder("<input type='submit' class='btn btn-primary' name='" + name + "' value='" + value + "'/>") ;
-            return new HtmlString(btn.ToString());
+            return new HtmlContentBuilder();
         }
     }
 }
