050a9a066aa8e4ebd8eadc10ed8af7e866080941
Руслан Русланов <sharp_blade666@mail.ru>
2021-12-09 01:36:30 +0300

    CreateLabel,SplitByCamelCase

--- hw7/hw7/EditorTemplates/MyHtmlHelperEditorExtensions.cs
+++ hw7/hw7/EditorTemplates/MyHtmlHelperEditorExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -16,24 +17,64 @@ namespace hw7.wwwroot
             {
                 throw new ArgumentNullException(nameof(htmlHelper));
             }
-            //var typeModel = htmlHelper.ViewData.Model.GetType();
+            var typeModel = htmlHelper.ViewData.Model.GetType();
             IHtmlContentBuilder htmlRes = new HtmlContentBuilder();
-            //var model = htmlHelper.ViewData.Model;
-            //foreach (var property in typeModel.GetProperties())
-                //htmlRes.AppendHtml(AddHtmlContent(property, model));
-                //model нужна для сравнения и определения типа инпута
-            return htmlRes.AppendHtml(AddHtmlContent());
+            var model = htmlHelper.ViewData.Model;
+            foreach (var property in typeModel.GetProperties())
+                htmlRes.AppendHtml(AddHtmlContent(property, model));
+            return htmlRes;
         }
 
-        private static string AddHtmlContent()//PropertyInfo property, object model
+        private static string AddHtmlContent(PropertyInfo property, object model)
         {
-            var page = new StringBuilder("<div class='col-md-6'><label for='inputEmail4' class='form-label'>Эл. адрес</label><input type='email' class='form-control' id='inputEmail4' placeholder='email'></div>");
+            var div1 = new TagBuilder("div");
+            div1.MergeAttribute("class","col-12-");
+            div1.InnerHtml.AppendHtml(CreateLabel(property));//для Label нужно толкьо название свойства
+            div1.InnerHtml.AppendHtml(CreateInput(property, model));// но для правильного input нужна искомая модель
+            var page = new StringBuilder("<div class='col-12'><label for='inputEmail4' class='form-label'>Эл. адрес</label><input type='email' class='form-control' id='inputEmail4' placeholder='email'></div>");
             return page.ToString();
         }
 
+        
+
+        private static IHtmlContent CreateLabel(PropertyInfo property)
+        {
+            var label = new TagBuilder("label")
+            {
+                Attributes =
+                {
+                    {"for", property.Name},
+                    {"class", "form-label"}
+                }
+                
+            };
+            label.InnerHtml.AppendLine(DisplayName(property));
+            return label;
+        }
+
+        private static string DisplayName(MemberInfo prop)
+        {
+            var displayName = prop.GetCustomAttribute<DisplayNameAttribute>();
+            return displayName is null ? SplitByCamelCase(prop.Name) : displayName.DisplayName;
+        }
+        
+        private static string SplitByCamelCase(string str)
+        {
+            return str
+                .Skip(1)
+                .Aggregate($"{str.FirstOrDefault()}",
+                    (current, symbol) => current + (char.IsUpper(symbol) ? $" {symbol}" : symbol));
+        }
+
+        private static IHtmlContent CreateInput(PropertyInfo property, object model)
+        {
+            throw new NotImplementedException();
+        }
+        
         public static IHtmlContent CreateSubmit(this IHtmlHelper helper, string name, string value)
         {
-            var btn = new StringBuilder("<div class='col-12'><input type='submit' class='btn btn-primary' name='" + name + "' value='" + value + "'/></div>") ;
+            var btn = new StringBuilder("<div class='col-12'><input type='submit' class='btn btn-primary' name='"
+                                        + name + "' value='" + value + "'/></div>") ;
             return new HtmlString(btn.ToString());
         }
     }
