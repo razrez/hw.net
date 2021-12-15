1d2ac74be852ff23f65a68a45074cae7fe4a4ff4
Руслан Русланов <sharp_blade666@mail.ru>
2021-12-15 23:05:36 +0300

    fixed

--- hw8/hw8/Controllers/CalculatorController.cs
+++ hw8/hw8/Controllers/CalculatorController.cs
@@ -16,7 +16,7 @@ namespace hw8.Controllers
             "plus",
             "minus",
             "multiply",
-            $"divide"
+            "divide"
         };
 
         public CalculatorController(ILogger<CalculatorController> calculator, 
@@ -30,16 +30,10 @@ namespace hw8.Controllers
         {
             var isVal1Int = double.TryParse(val1, out var num1);
             var isVal2Int = double.TryParse(val2, out var num2);
-
             if (!isVal1Int || !isVal2Int) //if there are no int args
-            {
                 return "Please, enter numbers";
-            }
-            if (num2 == 0 && operation == "divide")
-            {
+            if (num2 == 0 && operation == "/")
                 return "divide by zero!";
-            }
-            
             return ExpectedOperation.Contains(operation) ? 
                 _calculate.Calculate(num1, operation, num2) : 
                 "Unsupported operation! \nSupported are: plus, minus, divide and multiply";
@@ -47,8 +41,8 @@ namespace hw8.Controllers
         public IActionResult Index()
         {
             return Content(
-                "Fill val1, operation(plus, minus, multiply, divide) and val2 here '/calculator/calculate?val1= &operation= &val2= '\n" +
-                "and add it to address line.");
+                "example for filling: " +
+                "https://localhost:5001/calculator/calculate?val1=3&operation=divide&val2=2");
         }
 
 
