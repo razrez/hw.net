7155c453c0696f85be22a31fc3aef41dcd7bf9fb
Руслан Русланов <sharp_blade666@mail.ru>
2021-12-15 19:46:11 +0300

    ICalculate.cs

---
+++ hw8/hw8/Services/Calculate.cs
@@ -0,0 +1,19 @@
+﻿namespace hw8.Services
+{
+    public class Calculate : ICalculateService
+    {
+        int ICalculateService.Calculate(int val1, string operation, int val2)
+        {
+            var result = 0;
+            result = operation switch
+            {
+                "+" => val1 + val2,
+                "-" => val1 - val2,
+                "*" => val1 * val2,
+                "/" => val1 / val2,
+                _ => result
+            };
+            return result;
+        }
+    }
+}
---
+++ hw8/hw8/Services/ICalculate.cs
@@ -0,0 +1,7 @@
+﻿namespace hw8.Services
+{
+    public interface ICalculateService
+    {
+        int Calculate(int val1, string operation, int val2);
+    }
+}
