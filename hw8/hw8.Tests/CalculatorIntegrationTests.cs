16323eea1f562bd1547da821ed50d47a310e8bbf
Руслан Русланов <sharp_blade666@mail.ru>
2021-12-18 00:08:58 +0300

    ITest

--- hw8/hw8.Tests/CalculatorIntegrationTests.cs
+++ hw8/hw8.Tests/CalculatorIntegrationTests.cs
@@ -1,7 +1,36 @@
-﻿namespace hw8.Tests
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Xunit;
+using System;
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+
+namespace hw8.Tests
 {
     public class CalculatorIntegrationTests
     {
-        
+        public class HostBuilder : WebApplicationFactory<Startup>
+        {
+            protected override IHostBuilder CreateHostBuilder() =>
+                Host
+                    .CreateDefaultBuilder()
+                    .ConfigureWebHostDefaults(builder => builder
+                        .UseStartup<Startup>()
+                        .UseTestServer());
+        }
+
+        public class ITest : IClassFixture<HostBuilder>
+        {
+            private readonly string url = "https://localhost:5001/calculator/calculate?";
+            private readonly HttpClient _client;
+            public ITest(HostBuilder factory)
+            {
+                _client = factory.CreateClient();
+            }
+            
+        }
     }
 }
