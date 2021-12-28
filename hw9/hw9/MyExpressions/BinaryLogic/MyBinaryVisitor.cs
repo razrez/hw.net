5dc223163ea42260d86b608096aaaea9c1ba3044
Руслан Русланов <sharp_blade666@mail.ru>
2021-12-28 18:50:30 +0300

    all logic is done without Controller and form

---
+++ hw9/hw9/MyExpressions/BinaryLogic/MyBinaryVisitor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace hw8.MyExpressions.BinaryLogic
+{
+    public class MyBinaryVisitor : ExpressionVisitor
+    {
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            var result = ProcessInParallel(node.Left, node.Right).Result;
+            return node.NodeType switch
+            {
+                ExpressionType.Add => Expression.Add(Expression.Constant(result[0]) ,Expression.Constant(result[1])),
+                ExpressionType.Subtract => Expression.Subtract(Expression.Constant(result[0]) ,Expression.Constant(result[1])),
+                ExpressionType.Multiply => Expression.Multiply( Expression.Constant(result[0]) ,Expression.Constant(result[1])),
+                ExpressionType.Divide => Expression.Divide(Expression.Constant(result[0]), Expression.Constant(result[1]))
+            };
+        }
+
+        private static async Task<double[]> ProcessInParallel(Expression left, Expression right)
+        {
+            return await Task.WhenAll(
+                Task.Run(() => Expression.Lambda<Func<double>>(left).Compile().Invoke()),
+                Task.Run(() => Expression.Lambda<Func<double>>(right).Compile().Invoke()));
+        }
+    }
+}
---
+++ hw9/hw9/MyExpressions/Parser.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace hw8.MyExpressions
+{
+    public static class Parser
+    {
+        private static readonly Regex _inputSplit = new ("(?<=[-+*/\\(\\)])|(?=[-+*/\\(\\)])");
+        private static readonly Regex _operand = new ("[0-9]+");
+        
+        private static readonly Dictionary<string, int> _PriorityOperator = new()
+        {
+            {"(", 1},
+            {")", 1},
+            {"-", 2},
+            {"+", 2},
+            {"*", 3},
+            {"/", 3}
+        };
+
+        public static string ToPostfix(string expression)
+        {
+            var operators = new Stack<string>();
+            var postfix = new Stack<string>();
+            foreach (var i in _inputSplit.Split(expression.Replace(" ", "+")))
+            {
+                switch (i)
+                {
+                    case "":
+                        continue;
+                    case "(":
+                        operators.Push(i);
+                        break;
+                    default:
+                    {
+                        if (_operand.IsMatch(i))
+                        {
+                            postfix.Push(i);
+                        }
+                        else if (i == ")")
+                        {
+                            while (operators.Peek() != "(")
+                            {
+                                PostfixPush(operators, postfix);
+                            }
+
+                            operators.Pop();
+                        }
+                        else
+                        {
+                            while (operators.Count > 0 && operators.Peek() != "(" &&
+                                   _PriorityOperator[i] <= _PriorityOperator[operators.Peek()])
+                            {
+                                PostfixPush(operators, postfix);
+                            }
+
+                            operators.Push(i);
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            while (operators.Count > 0)
+            {
+                PostfixPush(operators, postfix);
+            }
+
+            return postfix.Pop();
+        }
+
+        private static void PostfixPush(Stack<string> operators, Stack<string> postfix)
+        {
+            var op = operators.Pop();
+            var first = postfix.Pop();
+            var second = postfix.Pop();
+            postfix.Push(second + " " + first + " " + op);
+        }
+    }
+}
