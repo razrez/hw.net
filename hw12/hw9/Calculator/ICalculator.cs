0b2ee36fc7753d929d7eda3a1172a895b9833b14
Руслан Русланов <sharp_blade666@mail.ru>
2021-12-30 08:28:22 +0300

    keep going

---
+++ hw12/hw9/Calculator/ICalculator.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace hw9.Calculator
+{
+    public interface ICalculator
+    {
+        public string Calculate(Expression node);
+        public string CalculateNew(Expression input);
+    }
+}
